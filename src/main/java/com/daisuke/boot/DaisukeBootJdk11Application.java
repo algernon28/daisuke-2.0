c7d5cb8f469d4c52b82a2b5d73892133a36c61aa
andrea.missinato <andrea.missinato@spindox.it>
2019-10-03 18:10:35 +0200

    domain model

---
+++ src/main/java/com/daisuke/boot/DaisukeBootJdk11Application.java
@@ -0,0 +1,13 @@
+package com.daisuke.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisukeBootJdk11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisukeBootJdk11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/daisuke/boot/DaisukeBootJdk11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daisuke.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisukeBootJdk11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
